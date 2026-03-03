9d6cd42823b0569fd656a3d6a282f1d533a2b9e6
Dilpreet <dschana6@pm.me>
2026-03-03 09:54:38 -0500

    tmux config

--- dschana/home.nix
+++ dschana/home.nix
@@ -9,6 +9,7 @@
   home.file = {
     ".oh-my-zsh".source = "${pkgs.oh-my-zsh}/share/oh-my-zsh";
     ".zshrc".source = ./zshrc;
+    ".tmux.conf".source = ./tmux.conf;
     ".config" = {
       source = ./config;
       recursive = true;
