8a8546b13de10e1eff19c310a5fbfdc4f0df76c9
Dilpreet <dschana6@pm.me>
2026-05-12 17:14:53 -0400

    format and llama

--- dev-machines/dschana-desktop/configuration.nix
+++ dev-machines/dschana-desktop/configuration.nix
@@ -1,58 +1,65 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+    imports = [
+        ./hardware-configuration.nix
+    ];
 
-  boot.resumeDevice = "/dev/disk/by-uuid/12adbf25-57dc-4572-99ff-41705534156a";
+    boot.resumeDevice = "/dev/disk/by-uuid/12adbf25-57dc-4572-99ff-41705534156a";
 
-  hardware.graphics.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    powerManagement.enable = true;
-  };
-
-  networking.hostName = "dschana-desktop";
-
-  home-manager.users.dschana = import ./home.nix;
-
-  environment.systemPackages = with pkgs; [
-    brave
-    davinci-resolve
-    freecad
-    krita
-    kicad
-    lmstudio
-    vulkan-tools
-  ];
-
-  programs.appimage.enable = true;
-  programs.appimage.binfmt = true;
-
-  ### Locally hosted services ###
-
-  services.flatpak.enable = true;
-
-  services.ollama = {
-    enable = true;
-    package = pkgs.ollama-cuda;
-    environmentVariables = {
-      OLLAMA_FLASH_ATTENTION = "1";
-      OLLAMA_KV_CACHE_TYPE = "q8_0";
-      OLLAMA_KEEP_ALIVE = "-1";
-      OLLAMA_CONTEXT_LENGTH="60000";
+    hardware.graphics.enable = true;
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+        modesetting.enable = true;
+        open = true;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        powerManagement.enable = true;
     };
-  };
 
-  services.open-webui = {
-    enable = true;
-    port = 9999;
-  };
+    networking.hostName = "dschana-desktop";
 
-  system.stateVersion = "25.11";
+    home-manager.users.dschana = import ./home.nix;
+
+    environment.systemPackages = with pkgs; [
+        brave
+            davinci-resolve
+            freecad
+            krita
+            kicad
+            lmstudio
+            vulkan-tools
+    ];
+
+    programs.appimage.enable = true;
+    programs.appimage.binfmt = true;
+
+    ### Locally hosted services ###
+
+    services.flatpak.enable = true;
+
+    #services.llama-cpp = {
+    #    enable = true;
+    #    package = pkgs.llama-cpp-vulkan;
+    #    port = 8181;
+    #    modelsDir = "/home/dschana/models/";
+    #};
+
+    services.ollama = {
+        enable = true;
+        package = pkgs.ollama-cuda;
+        environmentVariables = {
+            OLLAMA_FLASH_ATTENTION = "1";
+            OLLAMA_KV_CACHE_TYPE = "q8_0";
+            OLLAMA_KEEP_ALIVE = "-1";
+            OLLAMA_CONTEXT_LENGTH="60000";
+        };
+    };
+
+    services.open-webui = {
+        enable = true;
+        port = 9999;
+    };
+
+    system.stateVersion = "25.11";
 }
